f37f99854059fb06a56b50fd4134d02db5a097a4
odjacobs <98474660+odjacobs@users.noreply.github.com>
2022-05-09 17:07:44 -0400

    Debloat and Tweaks

--- debloat.ps1
+++ debloat.ps1
@@ -2,7 +2,7 @@
 # Purpose: Remove pre-installed garbage from Windows 10
 # Author: odjacobs
 
-$bloatware = @{
+$bloatware = @(
     # Microsoft crap
     "Microsoft.MinecraftUWP"
     "Microsoft.GamingServices"
@@ -42,8 +42,58 @@ $bloatware = @{
     "*HiddenCity*"
     "*AdobePhotoshopExpress*"
     "*HotspotShieldFreeVPN*"
-}
+    "*Disney*"
+    "*PicsArt*"
+    "*PolarrPhotoEditorAcademicEdition*"
+    "*AutodeskSketchBook*"
+    "*DisneyMagicKingdoms*"
+    "*MarchofEmpires*"
+    "*Plex*"
+    "*iHeartRadio*"
+    "*FarmVille*"
+    "*Duolingo*"
+    "*CyberLink*"
+    "*DolbyAccess*"
+    "*Drawboard*"
+    "*Fitbit*"
+    "*GAMELOFTSA*"
+    "*KeeperSecurityInc*"
+    "*NORDCURRENT*"
+    "*PandoraMediaInc"
+    "*Playtika*"
+    "*Shazam*"
+    "*SlingTV*"
+    "*Spotify*"
+    "*ThumbmunkeysLtd*"
+    "*TuneIn*"
+    "*WinZip*"
+    "*XING*"
+    "*flaregamesGmbH*"
+    "*king.com.*"
+    "TheNewYorkTimes.NYTCrossword"
+)
 
 foreach ($bloat in $bloatware) {
     Get-AppxPackage -Name $bloat | Remove-AppxPackage
 }
+
+# TODO: Implement unpinning the garbage from start menu
+
+
+# Registry tweaks to turn off Microsoft's nonsense
+# Disable Telemetry
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+
+# Disable Advertising ID
+If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo")) {
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" -Name "Enabled" -Type DWord -Value 0
+
+# Sticky keys, task view
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\StickyKeys" -Name "Flags" -Type String -Value "506"
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Type DWord -Value 0
+
+# Prevents "Suggested Applications" returning
+New-FolderForced -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" "DisableWindowsConsumerFeatures" 1
---
+++ tweaks.ps1
@@ -0,0 +1,25 @@
+# tweaks.ps1
+# Purpose enable needed settings and UX tweaks
+# Author: odjacobs
+
+
+# Allow Remote Desktop and Remote Assistance
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Remote Assistance" -Name "fAllowToGetHelp" -Type DWord -Value 1
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Type DWord -Value 1
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "UserAuthentication" -Type DWord -Value 1
+
+# File name extensions
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Type DWord -Value 0
+
+# Set Windows to Dark Mode #
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v "AppsUseLightTheme" /t "REG_DWORD" /d "0" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v "SystemUsesLightTheme" /t "REG_DWORD" /d "0" /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t "REG_DWORD" /d "0" /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t "REG_DWORD" /d "0" /f
+reg add "HKEY_USERS\.DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /f
+reg add "HKEY_USERS\.DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /f
+reg add "HKEY_USERS\.DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t "REG_DWORD" /d "0" /f
+reg add "HKEY_USERS\.DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t "REG_DWORD" /d "0" /f
